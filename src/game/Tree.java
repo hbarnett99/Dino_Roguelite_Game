ac832f76d2f8b1a0c82afcca9d844a43299792e1
Henry Barnett <hbar0010@student.monash.edu>
2019-10-04 20:39:19 +1000

    Renabled Tree Growth

--- src/game/Tree.java
+++ src/game/Tree.java
@@ -20,11 +20,11 @@ public class Tree extends Ground {
 	public void tick(Location location) {
 		super.tick(location);
 
-		//age++;
-		//if (age == 10)
-		//	displayChar = 't';
-		//if (age == 20)
-		//	displayChar = 'T';
+		age++;
+		if (age == 10)
+			displayChar = 't';
+		if (age == 20)
+			displayChar = 'T';
 		
 		//location.map().at(x, y)
 		
