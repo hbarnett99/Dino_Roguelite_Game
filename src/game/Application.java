55608d960c8f1ef6819f848be742f347e59118d5
tbil0002 <55126015+tbil0002@users.noreply.github.com>
2019-10-04 23:07:47 +1000

    Update Application.java
    FIXED

--- src/game/Application.java
+++ src/game/Application.java
@@ -53,28 +53,22 @@ public class Application {
 		Actor player = new Player("Player", '@', 100);
 		world.addPlayer(player, gameMap.at(8, 4));
 		
-		// Place a pair of protoceratops in the middle of the map
 		Protoceratops proto1 = new Protoceratops("Protoceratops");
+		gameMap.at(40, 5).addActor(proto1);
 		gameMap.at(40, 10).addActor(proto1);
 		gameMap.at(32, 12).addActor(new Protoceratops("Protoceratops"));
 		
 		//adds Velociraptors 
 		Velociraptor veloc1 = new Velociraptor("Velociraptor");
-<<<<<<< HEAD
+		gameMap.at(25, 20).addActor(veloc1);
 		veloc1.actionFactories.add(new HuntBehaviour());
 		veloc1.actionFactories.add(new WanderBehaviour());
 		gameMap.at(30, 12).addActor(veloc1);
 		//gameMap.at(10, 15).addActor(new Velociraptor("Velociraptor"));	
 		
 		//adds an egg
-		gameMap.at(8, 14).addItem(new Egg(DinosaurType.PROTOCERATOPS));
 		
-=======
-		veloc1.actionFactories.add(new HuntBehaviour(proto1));
-		veloc1.actionFactories.add(new FollowBehaviour(proto1));
-		gameMap.at(25, 20).addActor(veloc1);
-		//gameMap.at(10, 15).addActor(new Velociraptor("Velociraptor"));
->>>>>>> 9c0b24fb97dcea169c814aa97a6dba0c48f4f37d
+		
 		
 		world.run();
 	}
